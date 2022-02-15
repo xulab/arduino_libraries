c06719b16d34aafe84878b799b5aa0954f651fc3
panjingwei1945 <panjingwei1945@hotmail.com>
2022-02-15 16:20:19 +0800

    SPLC can use a external data now.

--- SPLC/SPLC.h
+++ SPLC/SPLC.h
@@ -12,8 +12,19 @@
 	#include "WProgram.h"
 #endif
 
+struct CALIB_STCT
+{
+	float* CALIB_Array;
+	float f0;
+	float df;
+	float calib_vol;
 
+};
 
+// extern float* addr_CALIB;
+extern struct CALIB_STCT calib_default_stct1;
+// extern float* addr_CALIB2;
+extern struct CALIB_STCT calib_default_stct2;
 
 
 typedef class SPLCClass
@@ -21,7 +32,10 @@ typedef class SPLCClass
 private:
 
 public:
-	int get_D_SPL(float fq, byte DB);	
+	// int get_D_SPL(float fq, byte DB, float* addr = addr_CALIB);
+	int SPLCClass::get_D_SPL(float fq, byte DB, struct CALIB_STCT param = calib_default_stct1);
+	// int get_D_SPL2(float fq, byte DB, float* addr = addr_CALIB2);	
+	int SPLCClass::get_D_SPL2(float fq, byte DB, struct CALIB_STCT param = calib_default_stct2);
 	void init();
 }SPLCClass;
 
